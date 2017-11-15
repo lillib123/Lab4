d92ba1930e5750d29bccbfad6ff0a07dec6552bf
lillib123 <lblair2@emich.edu>
2017-11-14 19:25:16 -0500

    lab 4 part 1

---
+++ controller.js
@@ -0,0 +1,18 @@
+(function() {
+  function lab4_controller() {
+    var vm = this;
+    vm.list = ["Grovery Shop", "Laundry", "Get Gas", "Vacuum"];
+
+    vm.add_item = function(item) {
+      vm.list.push(item);
+    };
+
+    vm.remove_item = function(item) {
+      vm.list.splice(item, 1);
+    };
+
+  }
+  angular
+  .module("lab4_module")
+  .controller("lab4_controller", lab4_controller);
+})();
