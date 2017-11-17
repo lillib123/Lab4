0357ccce5d54ccdd31fb23d19fad3f7a5282b596
lillib123 <lblair2@emich.edu>
2017-11-17 08:29:29 -0500

    fixed components

--- task-form.js
+++ task-form.js
@@ -1,3 +1,4 @@
+(function(){
 var taskForm = {
   controller: "lab4_controller",
   template: `
@@ -6,9 +7,8 @@ var taskForm = {
     <h1>TODO List</h1>
     <h2>A place to store the things you have to do!</h2>
     <input ng-model="filter" type="text" placeholder="Search List"></input>
-
     <ol>
-      <li hover-state ng-repeat="number in controller.list | filter:filter track by $index">{{number}} <a ng-click="controller.remove_item($index)" href=""><img src = "images/close.png"></a> </li>
+      <task-list list="$ctrl.list"></task-list>
     </ol>
     <input ng-model="controller.add_items" type="text" placeholder="Item to Add"></input>
     <button ng-click="controller.add_item(controller.add_items)">Add</button>
@@ -18,3 +18,4 @@ var taskForm = {
 angular
 .module("lab4_module")
 .component("taskForm", taskForm);
+})();
